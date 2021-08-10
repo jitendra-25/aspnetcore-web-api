c3f8048d2c362195d647a7f28c498cd82a17bf84
jitendra <jitendra.panchal25@gmail.com>
2021-08-10 12:41:28 +0530

    Add SeriLog configuration in appsettings file and read from there in Program.cs

--- MyBooks/Program.cs
+++ MyBooks/Program.cs
@@ -16,9 +16,17 @@ namespace MyBooks
         {
             try
             {
+                var configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
                 Log.Logger = new LoggerConfiguration()
-                    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+                    .ReadFrom.Configuration(configuration)
                     .CreateLogger();
+
+                //Log.Logger = new LoggerConfiguration()
+                //    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+                //    .CreateLogger();
 
                 CreateHostBuilder(args).Build().Run();
             }
